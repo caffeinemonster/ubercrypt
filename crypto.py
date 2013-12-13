ed69cdbdd0ff230131294206d0264327c9b92e0c
Tea <hakology@gmail.com>
2013-12-13 23:17:55 +0000

    Multiple IV support added.

--- crypto.py
+++ crypto.py
@@ -1,9 +1,22 @@
 #Licenced under the GNU General Public License. (V3)
 #Designed and coded by caffeinemonster
-#Copyright (2013) J. Green
+#Copywrite (2013) J. Green
 #please read the licence.txt file for more information
-import os, binascii, string, random, sys
+import os
+import binascii
+import string
+import random
+import sys
 from Crypto.Cipher import AES
+
+
+def func_GenIVs(intAmount):
+    print('Generating keys!')
+    arrIVs = [None] * (intAmount + 1)
+    for i in range(0, intAmount + 1):
+        arrIVs[i] = func_GenIV()
+    return arrIVs
+
 
 def func_GenIV():
     return func_GenKey(16, 1)
@@ -23,49 +36,62 @@ def func_GenKey(intdigits, method):
         strkey = "".join(oList)
         print (strkey)
         return strkey
-    elif method == 2:
+    elif method == 2:  # shit do not use
         strkey = binascii.b2a_hex(os.urandom(intdigits))[:intdigits]
         print(strkey)
         return strkey
 
 
-def func_EncryptAES(key, strmessage):
-    obj = AES.new(key, AES.MODE_CBC, IV)
+def func_EncryptAES(key, strmessage, strIV):
+    obj = AES.new(key, AES.MODE_CBC, strIV)
     ciphertext = obj.encrypt(strmessage)
     return ciphertext
 
 
-def func_DecryptAES(strkey, strencrypted):
-    cipher = AES.new(strkey, AES.MODE_CBC, IV)
+def func_DecryptAES(strkey, strencrypted, strIV):
+    cipher = AES.new(strkey, AES.MODE_CBC, strIV)
     strdecodedtext = cipher.decrypt(strencrypted)
     return strdecodedtext
 
 
-def func_EncryptLayers(strmessage, keylist):
+def func_EncryptLayers(strmessage, keylist, ivlist):
     for i in range(0, intKeys, 1):
-        encryptedmessage = func_EncryptAES(keylist[i], strmessage)
-        strmessage = encryptedmessage
+        encmsg = func_EncryptAES(keylist[i], strmessage, ivlist[i])
+        strmessage = encmsg
     return strmessage
 
 
-def func_DecryptLayers(strmessage, keylist):
+def func_DecryptLayers(strmessage, keylist, ivlist):
     for i in range(intKeys, 0, -1):
-        decryptedmessage = func_DecryptAES(myKeys[i - 1], strmessage)
-        strmessage = decryptedmessage
+        decmsg = func_DecryptAES(myKeys[i - 1], strmessage, ivlist[i - 1])
+        strmessage = decmsg
     return strmessage
 
 
-def func_SaveKeys(strfile, keylist, strIV):
+def func_SaveKeys(strfile, keylist, ivlist):
     f = open(strfile, 'w')
     for i in range(0, intKeys, 1):
         f.write(keylist[i] + '\n')
-    f.write(strIV + '\n')
+    for i in range(0, intKeys, 1):
+        f.write(ivlist[i] + '\n')
     f.close()
 
 
 def func_LoadKeys(strfile):
     keylist = [line.strip() for line in open(strfile)]
     return keylist
+
+
+def func_LoadIVs(strfile):
+    intCount = 1
+    myIVs = []
+    with open(strfile) as f:
+        content = f.readlines()
+        for s in content:
+            if intCount > (intKeys):
+                myIVs.append(s.rstrip())
+            intCount += 1
+    return myIVs
 
 
 def func_PadMessage(strmessage, intblocksize):
@@ -75,24 +101,26 @@ def func_PadMessage(strmessage, intblocksize):
 
 
 def func_CharToBytes_rstr(strmessage):
-    sreturn = 'ENC'
+    strreturn = 'ENC'
     for c in list(strmessage):
-        sreturn += '-' + str(ord(c))
+        strreturn += '-' + str(ord(c))
         #print chr(ord(c))
-    return sreturn
+    return strreturn
 
 
 def func_BytesToChar_rstr(strmessage):
-    sreturn = ''
+    strreturn = ''
     for c in strmessage.split('-'):
         if c != 'ENC':
-            sreturn += str(chr(int(c)))
-    return sreturn
+            strreturn += str(chr(int(c)))
+    return strreturn
+
 
 def func_GenerateKeyFile():
     myKeys = func_GenKeys(intKeys)
-    IV = func_GenIV()
-    func_SaveKeys('ENCDEC.key', myKeys, IV)
+    #IV = func_GenIV()
+    myIVs = func_GenIVs(intKeys)
+    func_SaveKeys('ENCDEC.key', myKeys, myIVs)
     print(('Generated keys. (' + str(intKeys) + ')'))
     print(('Saved to file: ENCDEC.key'))
     print(('Carefully share this file.'))
@@ -124,27 +152,29 @@ if len(sys.argv) == 1:
     exit()
 
 #declare globals
-intKeys = 1024
+intKeys = 16
 myKeys = []
+myIVs = []
 action = 0
 intDoEncrypt = 0
 intDoDecrypt = 1
-intDoKeyList = 3
+intDoKeyList = 2
 
 for i in range(0, len(sys.argv)):
-    #print(('Argument:' + str(i) + sys.argv[i]))
     if sys.argv[i] == '-h':
         func_DisplayHelp()
         exit()
+
     elif sys.argv[i] == '-g':
         try:
-            intKeys = int(sys.argv[i + 1])
+            intKeys = int(sys.argv[int(i + 1)])
         except IndexError:
-            intKeys = 1024
+            intKeys = 16
         print(('Generating keys. (' + str(intKeys) + ')'))
         #generate keyfile
         func_GenerateKeyFile()
         exit()
+
     elif sys.argv[i] == '-e':
         try:
             somemessage = func_PadMessage(sys.argv[i + 1], 16)
@@ -152,6 +182,7 @@ for i in range(0, len(sys.argv)):
         except IndexError:
             print('No data to encrypt.')
             exit()
+
     elif sys.argv[i] == '-d':
         try:
             somemessage = func_BytesToChar_rstr(sys.argv[i + 1])
@@ -162,6 +193,7 @@ for i in range(0, len(sys.argv)):
         except IndexError:
             print('No data to decrypt.')
             exit()
+
     elif sys.argv[i] == '-k':
         action = intDoKeyList
 
@@ -172,28 +204,31 @@ if not os.path.isfile('ENCDEC.key'):
 #if the keys havent just been generated then load them
 if len(myKeys) == 0:
     myKeys = func_LoadKeys('ENCDEC.key')
+    intKeys = int(len(myKeys) / 2)
+    myIVs = func_LoadIVs('ENCDEC.key')
     if len(myKeys) == 0:
         print('Error loading key file.')
         exit()
+#calculate the amount of keys generated / loaded
+intKeys = int(len(myKeys) / 2)
 
-#set globals according to key file
-intKeys = len(myKeys) - 1
-IV = myKeys[len(myKeys) - 1]
-
-
+#performa action
 if (action == intDoEncrypt):
     #encrypt message
-    somemessage = func_EncryptLayers(somemessage, myKeys)
-    print(('ENC(' + str(intKeys) + '):' + func_CharToBytes_rstr(somemessage)))
+    somemessage = func_EncryptLayers(somemessage, myKeys, myIVs)
+    print((func_CharToBytes_rstr(somemessage)))
     exit()
+
 elif (action == intDoDecrypt):
     #decrypt message
-    somemessage = func_DecryptLayers(somemessage, myKeys)
+    somemessage = func_DecryptLayers(somemessage, myKeys, myIVs)
     print(('DEC(' + str(intKeys) + '):' + somemessage))
     exit()
+
 elif action == intDoKeyList:
     #output key
     for i in range(0, intKeys, 1):
         print((myKeys[i]))
-    print(IV)
+    for i in range(0, intKeys, 1):
+        print((myIVs[i]))
     exit()
